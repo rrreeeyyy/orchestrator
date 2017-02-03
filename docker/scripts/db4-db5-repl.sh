32586f0df6167f519b6d2faae6078b212b04f2c8
Ryota Yoshikawa <ryota-yoshikawa@cookpad.com>
2017-02-03 09:06:27 +0900

    Add docker-compose and replication components

---
+++ docker/scripts/db4-db5-repl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mysql -uroot -ss -e 'GRANT REPLICATION SLAVE ON *.* TO "repl"@"%" IDENTIFIED BY "p@ssw0rd"' --host 127.0.0.1 --port 43306 -pp3rc0na
+mysql -uroot -ss -e 'CHANGE MASTER TO MASTER_HOST="db4", MASTER_USER="repl", MASTER_PASSWORD="p@ssw0rd", MASTER_CONNECT_RETRY=10, MASTER_RETRY_COUNT=36' --host 127.0.0.1 --port 53306 -pp3rc0na
+mysql -uroot -ss -e 'START SLAVE' --host 127.0.0.1 --port 53306 -pp3rc0na
